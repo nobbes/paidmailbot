eabacfbb891985e500e8dafaf0ce606e4c0dbfb2
Nergon <nergonclient@gmail.com>
2019-07-27 18:17:41 +0200

    Update main.py
    Added new Paidmail Providers
    +dondino
    +Bonus Bunny
    +Questler

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import easyimap
 import re
 import requests
-from fake_useragent import UserAgent
 import time
 
 
 class Bot:
-    login = 'your_email@provider.com'
-    password = 'your_password'
+    login = 'your@email.com'
+    password = 'yourpassword'
     read_mails = []
     imapper = None
-    user_agent = "" ""
     accept_language = 'de-DE,de;q=0.5'
 
     def __init__(self):
-        fake_ua = UserAgent()
-        self.user_agent = str(fake_ua.random)
         self.s = requests.session()
         self.connect()
         print("[*]Add all emails to read Array")
@@ -25,7 +21,7 @@ class Bot:
 
     def connect(self):
         print("[+](Re-)Connecting to IMAP Server")
-        self.imapper = easyimap.connect('imap.your_server.com', self.login, self.password)
+        self.imapper = easyimap.connect('mail.yourmailserver.com', self.login, self.password)
 
     def read_emails(self):
         print("[*]Reading Emails")
@@ -33,7 +29,7 @@ class Bot:
             if mail_id in self.read_mails:
                 continue
             self.read_mails.append(mail_id)
-            mail = self.imapper.mail(mail_id)
+            mail = self.imapper.mail(mail_id, include_raw=True)
             if "Qassa" in mail.from_addr:
                 print("[+]Found new Mail from Qassa")
                 try:
@@ -52,15 +48,37 @@ class Bot:
                     print("[+]Visited link")
                 except:
                     print("[-]No Url found]")
+            if "dondino" in mail.from_addr.lower():
+                print("[+]Found new Mail from DonDino")
+                try:
+                    url = re.search("(?P<url>http://www.dondino.de/[^\s]+)", mail.body).group("url")
+                    visit_link = self.s.get(url, allow_redirects=True)
+                    print("[+]Visited link")
+                except:
+                    print("[-]No Url found]")
+            if "Bonus Bunny" in mail.from_addr:
+                print("[+]Found new Mail from Bonus Bunny")
+                try:
+                    url = re.search("(?P<url>https://www.bonus-bunny.de/pmail.php[^\s]+)", mail.raw).group("url")
+                    url = url.replace("\"", "")
+                    visit_link = self.s.get(url, allow_redirects=True)
+                    print("[+]Visited link")
+                except:
+                    print("[-]No Url found]")
+            if "Questler.de" in mail.from_addr:
+                print("[+]Found new Mail from Questler")
+                try:
+                    url = re.search("(?P<url>https://www.questler.de/earn/best.php[^\s]+)", mail.body).group("url")
+                    visit_link = self.s.get(url, allow_redirects=True)
+                    print("[+]Visited link")
+                except:
+                    print("[-]No Url found]")
         print("[*]Sleeping 1 Hour")
         time.sleep(3600)
         self.connect()
         self.read_emails()
 
 
-def main():
+if __name__ == '__main__':
     bot = Bot()
 
-
-if __name__ == '__main__':
-    main()
